c710f1c1e488e197fa9a4bec71afdda2972ec9dd
davidsgc <davidsgc@rose-hulman.edu>
2019-03-11 14:26:00 -0400

    DONE m2

--- src/m3_capital_t_class.py
+++ src/m3_capital_t_class.py
@@ -3,7 +3,7 @@ A   CapitalT   class and functions that use/test it.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
+         and Gabby Davidson.
 """  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
