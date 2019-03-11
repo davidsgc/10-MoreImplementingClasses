bf63790fdf546deef99a2ed1895fd612036dad4a
davidsgc <davidsgc@rose-hulman.edu>
2019-03-11 14:25:25 -0400

    DONE m2

--- src/m2_baby_class.py
+++ src/m2_baby_class.py
@@ -3,8 +3,8 @@ A   Baby   class and functions that use/test it.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Gabby Davidson.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -107,7 +107,29 @@ def main():
 #          """ Brief description of what an object of the class 'is'. """
 #
 ###############################################################################
+class Baby(object):
 
+    def __init__(self, name):
+        self.babyname = name
+        self.countfood = 0
+        self.counttime = 0
+        print("Hello baby", self.babyname)
+
+    def feed_baby(self):
+        print("Thank you for feeding baby", self.babyname)
+        self.countfood = self.countfood + 1
+
+    def hour_passes(self):
+        if self.countfood == 1:
+            self.countfood = 0
+            self.counttime = 0
+        self.counttime = self.counttime + 1
+        if self.counttime == 1:
+            print("Baby", self.babyname, "is sleeping.")
+        if self.counttime == 2:
+            print("Baby", self.babyname, "is awake. Time for food.")
+        if self.counttime >= 3:
+            print("Baby", self.babyname, "is CRYING uncontrollably! Feed the baby!")
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
